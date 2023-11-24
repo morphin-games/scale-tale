057d2aebce2ff445f05b7ef126c498ee0ff3e40a
Saúl <saulespanaara3@gmail.com>
2023-11-25 00:32:26 +0100

    Coin and damage sfx

--- scripts/classes/collectible_coin.gd
+++ scripts/classes/collectible_coin.gd
@@ -9,7 +9,7 @@ func _on_player_detect_area_3d_player_entered(player : SPPlayer3D) -> void:
 	coin_sfx_data.volume_db = -8.0
 	coin_sfx_data.max_db = -3.0
 	coin_sfx_data.sound_start = 0
-	Utils.play_3d_sound_at(load("res://art/sfx/collectibles/coin.mp3"), global_transform.origin, get_parent(), coin_sfx_data)
+	Utils.play_3d_sound_at(load("res://art/sfx/collectibles/coinv2.mp3"), global_transform.origin, get_parent(), coin_sfx_data)
 	(Persistance.persistance_data as PersistanceData).coin_recount += 1
 	Persistance.save()
 	var inst : GPUParticlesIPOS3D = ipos.instantiate()
