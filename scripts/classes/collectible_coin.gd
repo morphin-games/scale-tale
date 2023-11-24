640e0786c78a90e4fe199292fff6af3655b3407d
Saúl <saulespanaara3@gmail.com>
2023-11-24 19:11:20 +0100

    SFX

--- scripts/classes/collectible_coin.gd
+++ scripts/classes/collectible_coin.gd
@@ -5,11 +5,11 @@ var ipos : PackedScene = preload("res://scenes/particle_effects/coin_taken.tscn"
 
 func _on_player_detect_area_3d_player_entered(player : SPPlayer3D) -> void:
 	var coin_sfx_data : AudioStream3DData = AudioStream3DData.new()
-	coin_sfx_data.pitch_scale = 1.5
+	coin_sfx_data.pitch_scale = 1
 	coin_sfx_data.volume_db = -8.0
 	coin_sfx_data.max_db = -3.0
-	coin_sfx_data.sound_start = 0.18
-	Utils.play_3d_sound_at(load("res://art/sfx/collectibles/chiclin.mp3"), global_transform.origin, get_parent(), coin_sfx_data)
+	coin_sfx_data.sound_start = 0
+	Utils.play_3d_sound_at(load("res://art/sfx/collectibles/coin.mp3"), global_transform.origin, get_parent(), coin_sfx_data)
 	(Persistance.persistance_data as PersistanceData).coin_recount += 1
 	Persistance.save()
 	var inst : GPUParticlesIPOS3D = ipos.instantiate()
