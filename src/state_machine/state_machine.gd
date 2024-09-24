a94f2547b78c0c80fc9225303aa7be138abac600
PolanskiPol <polanski.real@gmail.com>
2024-09-24 20:58:37 +0200

    feat: State Machine for Pawn States

---
+++ src/state_machine/state_machine.gd
@@ -0,0 +1,28 @@
+class_name StateMachine
+extends Node
+
+@export var states : Array[State]
+
+@onready var state : State = states[0] if states.size() > 0 else null : 
+	set(new_state):
+		state.active = false
+		state = new_state
+		state.enter()
+		state.active = true
+
+func _ready() -> void:
+	setup()
+
+func setup() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	print(state.resource_path)
+	for iterated in states:
+		if(iterated.enter_condition()):
+			state = iterated
+			break
+	
+	if(state == null): return
+	
+	state.process(delta)
