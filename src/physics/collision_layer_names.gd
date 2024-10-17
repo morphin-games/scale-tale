b56038272355121b7ee9b6e1c595430aa4699333
PolanskiPol <polanski.real@gmail.com>
2024-10-17 13:57:37 +0200

    chore: Readd collision_layer_names

---
+++ src/physics/collision_layer_names.gd
@@ -0,0 +1,17 @@
+@tool
+class_name CollisionLayerNames
+extends Object
+
+static var PLAYER : int = 1
+static var SOLID : int = 2
+static var ENEMY : int = 4
+static var WATER : int = 8
+static var SCALE_PROJECTILE : int = 16
+static var SCALABLE : int = 32
+static var SCALABLE_ATTRACTION_FIELD : int = 64
+static var RIGID : int = 128
+static var GRABABBLE : int = 256
+static var GRABBER : int = 512
+static var MOVABLE : int = 1024
+#static var EMPTY : int = 2048
+#static var EMPTY : int = 4096
